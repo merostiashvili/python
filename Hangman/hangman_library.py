adcf41f65b2001a73bafa320626ac3027ef7791e
Merab Rostiashvili <merostiashvili@tbcbank.com.ge>
2024-11-25 00:24:58 +0400

    დასრულდა ჰანგმან-ის პროექტი

---
+++ Hangman/hangman_library.py
@@ -0,0 +1,83 @@
+list_of_words = [
+    "apple", "banana", "orange", "grape", "pear",
+    "lemon", "lime", "kiwi", "mango", "peach",
+    "plum", "cherry", "berry", "melon", "watermelon",
+    "pineapple", "coconut", "strawberry", "raspberry", "blueberry",
+    "blackberry", "cranberry", "currant", "gooseberry", "cloudberry",
+    "boysenberry", "loganberry", "youngberry", "mulberry", "redcurrant",
+    "whitecurrant", "blackcurrant", "dogwood", "elderberry", "juniper",
+    "huckleberry", "lingonberry", "saskatoon berry", "serviceberry", "chokeberry",
+    "redbud", "hawthorn", "rowan", "mountain ash", "crabapple",
+    "quince", "fig", "date", "olive", "avocado",
+    "guava", "papaya", "dragonfruit", "lychee", "durian",
+    "mangosteen", "rambutan", "jackfruit", "kumquat", "persimmon",
+    "pomegranate", "date", "apricot", "nectarine", "honeydew",
+    "cantaloupe", "casaba", "crenshaw", "muskmelon", "winter melon",
+    "bottle gourd", "pumpkin", "squash", "zucchini", "cucumber",
+    "broccoli", "cauliflower", "kale", "spinach", "lettuce",
+    "carrot", "potato", "onion", "garlic", "pepper",
+    "tomato", "corn", "pea", "bean", "lentil",
+    "rice", "wheat", "barley", "oats", "rye",
+    "sorghum", "millet", "teff", "quinoa", "buckwheat",
+    "amaranth", "chia", "flax", "hemp", "sesame",
+    "almond", "walnut", "pecan", "hazelnut", "pistachio",
+    "cashew", "peanut", "sunflower seed", "pumpkin seed", "sesame seed",
+    "coffee", "tea", "cocoa", "chocolate", "vanilla",
+    "cinnamon", "ginger", "nutmeg", "clove", "cardamom", "cumin", "coriander", "paprika", "cayenne pepper"
+]
+main_picture = '''
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _' | '_ \ / _' | '_ ' _ \ / _' | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/
+'''
+lives = [""" +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+****************************Game Over****************************""", """ +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+****************************1/6 LIVES LEFT****************************""", """ +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+****************************2/6 LIVES LEFT****************************""", """  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+****************************3/6 LIVES LEFT**************************** """, """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+****************************4/6 LIVES LEFT****************************""", """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+****************************5/6 LIVES LEFT****************************"""
+]
